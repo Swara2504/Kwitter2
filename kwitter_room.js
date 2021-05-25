34db2bdeffacb2c468660c9476a9b0e891580243
Swara2504 <75523745+Swara2504@users.noreply.github.com>
2021-05-25 13:45:41 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyB-z1DNraVVIoY8ki_TU_1Ikkv4x-VQ1K0",
+    authDomain: "kwitterproject-6c918.firebaseapp.com",
+    projectId: "kwitterproject-6c918",
+    storageBucket: "kwitterproject-6c918.appspot.com",
+    messagingSenderId: "258763588269",
+    appId: "1:258763588269:web:56436ebec0281b7fdf7c24"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
